aa60c558db5fc83df867226290b834caa78afe92
Federico Balderas <federico.balderas@openpay.mx>
2016-01-12 13:46:09 -0600

    Actualización de un cargo

--- resources/OpenpayCharge.php
+++ resources/OpenpayCharge.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /**
  * Openpay API v1 Client for PHP (version 1.0.0)
  * 
@@ -6,40 +7,49 @@
  * http://www.openpay.mx/
  * soporte@openpay.mx
  */
+class OpenpayCharge extends OpenpayApiResourceBase
+{
 
-class OpenpayCharge extends OpenpayApiResourceBase {
-	protected $authorization;
-	protected $creation_date;
-	protected $currency;
-	protected $customer_id;
-	protected $operation_type;
-	protected $status;
-	protected $transaction_type;
+    protected $authorization;
+    protected $creation_date;
+    protected $currency;
+    protected $customer_id;
+    protected $operation_type;
+    protected $status;
+    protected $transaction_type;
+    // temporal hack
+    // TODO: checar porque no instancia Openpaycard al recibir el parametro
+    protected $card;
+    protected $derivedResources = array('Refund' => null, 'Capture' => null);
 
-	// temporal hack
-	// TODO: checar porque no instancia Openpaycard al recibir el parametro
-	protected $card;
+    public function refund($params) {
+        $resource = $this->derivedResources['refunds'];
+        if ($resource) {
+            return parent::_create($resource->resourceName, $params, array('parent' => $this));
+        }
+    }
 
-	protected $derivedResources = array('Refund' => null, 'Capture' => null);
+    public function capture($params) {
+        $resource = $this->derivedResources['captures'];
+        if ($resource) {
+            return parent::_create($resource->resourceName, $params, array('parent' => $this));
+        }
+    }
 
-	public function refund($params) {
-		$resource = $this->derivedResources['refunds'];
-		if ($resource) {
-			return parent::_create($resource->resourceName, $params, array('parent' => $this));
-		}
-	}
+    public function update($params) {
+        return $this->_updateCharge($params);
+    }
 
-	public function capture($params) {
-		$resource = $this->derivedResources['captures'];
-		if ($resource) {
-			return parent::_create($resource->resourceName, $params, array('parent' => $this));
-		}
-	}
 }
+
 // ----------------------------------------------------------------------------
-class OpenpayChargeList extends OpenpayApiDerivedResource {
-	public function create($params) {
-		return $this->add($params);
-	}
+class OpenpayChargeList extends OpenpayApiDerivedResource
+{
+
+    public function create($params) {
+        return $this->add($params);
+    }
+
 }
+
 ?>
