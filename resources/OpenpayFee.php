56ea0d9e09d3e3a55c13cf82999a09ee7fed2210
Federico Balderas <federico.balderas@openpay.mx>
2017-11-16 20:19:23 -0600

    Implementación de devolución de comisiones.

--- resources/OpenpayFee.php
+++ resources/OpenpayFee.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /**
  * Openpay API v1 Client for PHP (version 1.0.0)
  * 
@@ -6,25 +7,36 @@
  * http://www.openpay.mx/
  * soporte@openpay.mx
  */
+class OpenpayFee extends OpenpayApiResourceBase
+{
 
-class OpenpayFee extends OpenpayApiResourceBase {
-	protected $authorization;
-	protected $creation_date;
-	protected $currency;
-	protected $operation_type;
-	protected $status;
-	protected $transaction_type;
-	protected $error_message;
-	protected $method;
+    protected $authorization;
+    protected $creation_date;
+    protected $currency;
+    protected $operation_type;
+    protected $status;
+    protected $transaction_type;
+    protected $error_message;
+    protected $method;    
+    protected $derivedResources = array('Refund' => null);
+
+    public function refund($params) {
+        $resource = $this->derivedResources['refunds'];
+        if ($resource) {
+            return parent::_create($resource->resourceName, $params, array('parent' => $this));
+        }
+    }    
+
 }
+
 // ----------------------------------------------------------------------------
-class OpenpayFeeList extends OpenpayApiDerivedResource {
-	public function create($params) {
-		return $this->add($params);
-	}
-	public function get($params) {
-		// TODO: throw exception
-		return null;
-	}
+class OpenpayFeeList extends OpenpayApiDerivedResource
+{
+
+    public function create($params) {
+        return $this->add($params);
+    }
+
 }
+
 ?>
