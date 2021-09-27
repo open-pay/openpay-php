54ac577419fdbdbdd2c2162e7f2de3c0fddb4a14
Carlos Hernadnez Perez <carloshe@qrsof.com>
2021-09-27 11:46:55 -0500

    fix path resources

--- Openpay/Resources/OpenpayBine.php
+++ Openpay/Resources/OpenpayBine.php
@@ -1,8 +1,10 @@
 <?php
 
+use Openpay\Data\OpenpayApiResourceBase;
+
 /**
  * Openpay API v1 Client for PHP (version 2.0.1)
- * 
+ *
  * Copyright © Openpay SAPI de C.V. All rights reserved.
  * http://www.openpay.mx/
  * soporte@openpay.mx
